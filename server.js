e3f4f598066d7f943cc5f99bd82d2335f993cf1c
Yuki Miyake <miyake_yuuki320@yahoo.co.jp>
2024-02-03 23:08:49 +0900

    node server.jsで動く

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
+// const cors = require('cors'); // 追加
 const { exec } = require('child_process');
 
 const app = express();
 const port = 3000;
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 // CORS middleware
-app.use(cors());
+// app.use(cors());
 
 app.get('/execute-script', (req, res) => {
-  exec('bash scripts/createFile.sh', (error, stdout, stderr) => {
+  exec('bash scripts/script1.sh', (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing script: ${error}`);
       res.status(500).send('Internal Server Error');
